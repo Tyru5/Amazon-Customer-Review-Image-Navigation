51d15b3e12f64b55fcff028318aee53d77bc7bab
tiru5 <tiru5@DESKTOP-94R1FL3>
2020-10-09 12:15:02 -0700

    removed code that didnt work after more testing.

--- js/content.js
+++ js/content.js
@@ -4,19 +4,16 @@
  */
 
 // Register key-up events on the right / left / and Escape button to navigate the pop-up modal that amazon renders:
-// We only register these key-up events if the user has clicked / selected to view all customer images.
-jQuery('.reviews-image-gallery-link').click( function( event ) {
-    jQuery(window).keyup( function( e ) {
-        let key = e.key;
-        if( key === 'ArrowRight' ) {
-            jQuery('.next-button').click();
-            return false;
-        } else if( key === 'ArrowLeft' ) {
-            jQuery('.back-button').click();
-            return false;
-        } else if( key === 'Escape' ) {
-            jQuery('.a-button-close').click();
-            return false;
-        }
-    });
+jQuery( window ).keyup( function( e ) {
+    let key = e.key;
+    if( key === 'ArrowRight' ) {
+        jQuery('.next-button').click();
+        return false;
+    } else if( key === 'ArrowLeft' ) {
+        jQuery('.back-button').click();
+        return false;
+    } else if( key === 'Escape' ) {
+        jQuery('.a-button-close').click();
+        return false;
+    }
 });
