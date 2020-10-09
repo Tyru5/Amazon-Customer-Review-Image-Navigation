3e34e0096cb98cb910d89fefe2ffbd50162d4e50
tiru5 <tiru5@DESKTOP-94R1FL3>
2020-10-08 23:02:31 -0700

    Installed jQuery to help out with DOM manipulation, and resolved! There is probably a better way of implementing this, but I dont think it is too bad. Was my first chrome extension! Now I need linnea to create some icon assets!

--- js/content.js
+++ js/content.js
@@ -3,4 +3,17 @@
  * This code is licensed under MIT license (see LICENSE.txt for details)
  */
 
-alert('Hello World!');
+// Register key-up events on the right / left / and Escape button to navigate the pop-up modal that amazong renders:
+jQuery(window).keyup( function( e ) {
+    let key = e.key;
+    if( key === 'ArrowRight' ) {
+        jQuery('.next-button').click();
+        return false;
+    } else if( key === 'ArrowLeft' ) {
+        jQuery('.back-button').click();
+        return false;
+    } else if( key === 'Escape' ) {
+        jQuery('.a-button-close').click();
+        return false;
+    }
+});
